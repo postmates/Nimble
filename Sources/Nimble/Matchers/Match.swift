e4f0eecac7c2867a1719e2265f22dc1d54b1f931
Sho Ikeda <suicaicoca@gmail.com>
2018-06-21 22:33:02 +0900

    Refactor `match` matcher using `Predicate.define`

--- Sources/Nimble/Matchers/Match.swift
+++ Sources/Nimble/Matchers/Match.swift
@@ -3,17 +3,16 @@ import Foundation
 /// A Nimble matcher that succeeds when the actual string satisfies the regular expression
 /// described by the expected string.
 public func match(_ expectedValue: String?) -> Predicate<String> {
-    return Predicate.fromDeprecatedClosure { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "match <\(stringify(expectedValue))>"
-
+    return Predicate.define("match <\(stringify(expectedValue))>") { actualExpression, msg in
         if let actual = try actualExpression.evaluate() {
             if let regexp = expectedValue {
-                return actual.range(of: regexp, options: .regularExpression) != nil
+                let bool = actual.range(of: regexp, options: .regularExpression) != nil
+                return PredicateResult(bool: bool, message: msg)
             }
         }
 
-        return false
-    }.requireNonNil
+        return PredicateResult(status: .fail, message: msg)
+    }
 }
 
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
